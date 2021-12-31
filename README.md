c6a74827e792682ee52100c8d0284fcb67575141
jinyunjae <jinyun3075@naver.com>
2021-12-31 18:26:25 +0900

    add  Mongoose CRUD & init atlas

--- README.md
+++ README.md
@@ -120,3 +120,139 @@ router.get('/logout',(req,res)=>{
 
 module.exports = router;
 ```
+
+# NoSQL(not only Structure Query Language) - MongoDB
+1. SQL은 보통 스키마를 만들어 규칙과 일관성있게 데이터를 관리할 수 있다.
+2. NoSQL은 유연하고 빠른속도로 데이터를 관리할 수 있다.
+3. document Oriented (문서 지향)
+4. 예) collection = Document(Data)+Document(Data)+Document(Data)
+5. json 타입으로 저장
+6. Mongodb camppas : GUI로 관리 가능
+
+# ODM (Object Doucmnet Mapping) - mongoose 패키지
+1. npm i mongoose
+2. 객체와 문서를 1:1 매핑
+3. mongoose와 atals 연결
+* pakage bin/www
+``` js
+const dbconfig = require("...");
+
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+db.on("error", console.error); // 애러발생시
+db.once("open",()=> {     // 실행시
+  console.log("connected to mongodb server!!");
+})
+mongoose.connect( // atlas / mongoose 연결
+  `mongodb+srv://jin:${dbconfig.pw}@first-project.0dfpr.mongodb.net/${dbconfig.name}?retryWrites=true&w=majority`
+  ,{userNewUrlParser: true, useUnifiedTopology:true} // 경고문자 방지용 옵션
+)
+```
+
+# monggose
+1. Schema 사용예
+``` js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title : String,
+    content : String,
+    create_data: {type: Date, default: new Date()},
+});
+
+module.exports = mongoose.model("post", postSchema);
+```
+
+2. CRUD 서버 만들기
+```js
+const postModel = require("../model/post");
+
+//CREATE
+router.post('/',async (req,res)=> {
+  const {title,content} = req.body;
+  const post = new postModel({
+    title: title,
+    content: content,
+  });
+  try {
+    const result = await post.save();
+    res.status(200).json({
+      message:"upload success!!",
+      data: result,
+    });
+  } catch(error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
+});
+
+//READE
+router.get('/',async (req,res) => {
+  try {
+    const result = await postModel.find({}); //  중괄호에 찾을 목록 입력가능 예){title:"first"}
+    res.status(200).json({
+      message: "read success!!",
+      data : result,
+    });
+  } catch(error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
+})
+
+router.get('/:id',async (req,res)=> {
+  const { id } = req.params;
+  try {
+    const result = await postModel.findById(id);
+    res.status(200).json({
+      message:"detail success!!",
+      data: result,
+    });
+  } catch (error){
+    res.status(500).json({
+      message: error,
+    })
+  }
+})
+
+//UPDATE
+router.put('/:id',async (req,res)=> {
+  const {id}=req.params;
+  const {title, content} = req.body;
+  try {
+    const result = await postModel.findByIdAndUpdate(id,{
+      title: title,
+      content : content,
+    },{
+      new:true, // 변경된 사항으로 저장하는 옵션
+    });
+    res.status(200).json({
+      message: "update success!!",
+      data: result,
+    })
+  } catch(error) {
+    res.status(500).json({
+      message: error,
+    })
+  }
+})
+
+//DELETE
+router.delete('/:id',async (req,res)=> {
+  const {id} = req.params;
+  try {
+    const result =await postModel.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "delete success!!",
+      data: result
+    })
+  } catch(error) {
+    res.status(500).json({
+      message:error,
+    })
+  }
+})
+```
--- first-project/routes/index.js
+++ first-project/routes/index.js
@@ -23,6 +23,8 @@ var router = express.Router();
 //   res.render("index");
 // });
 
+// http 메소드
+
 // Get method
 // router.get('/read',(req,res)=>{
 //   res.status(200).json({
@@ -64,21 +66,110 @@ var router = express.Router();
 //   })
 // })
 
-const loginCheck = require("../module/loginCheck");
+//  이미지 업로드 % 로그인체크
 
-router.get('/',loginCheck,(req,res)=> {
-  res.status(200).json({
-    message: "login success",
-  });
-});
+// const loginCheck = require("../module/loginCheck");
 
-const upload = require("../module/imgUpload");
+// router.get('/',loginCheck,(req,res)=> {
+//   res.status(200).json({
+//     message: "login success",
+//   });
+// });
 
-router.post('/upload',upload.single('image'),(req,res) => {
-  const file = req.file;
-  console.log(file);
-  res.status(200).json({
-    message: "upload success!!",
-  })
-})
+// const upload = require("../module/imgUpload");
+
+// router.post('/upload',upload.single('image'),(req,res) => {
+//   const file = req.file;
+//   console.log(file);
+//   res.status(200).json({
+//     message: "upload success!!",
+//   })
+// })
+
+// const postModel = require("../model/post");
+
+// router.post('/',async (req,res)=> {
+//   const {title,content} = req.body;
+//   const post = new postModel({
+//     title: title,
+//     content: content,
+//   });
+//   try {
+//     const result = await post.save();
+//     res.status(200).json({
+//       message:"upload success!!",
+//       data: result,
+//     });
+//   } catch(error) {
+//     res.status(500).json({
+//       message: error,
+//     });
+//   }
+// });
+
+// router.get('/',async (req,res) => {
+//   try {
+//     const result = await postModel.find({}); //  중괄호에 찾을 목록 입력가능 예){title:"first"}
+//     res.status(200).json({
+//       message: "read success!!",
+//       data : result,
+//     });
+//   } catch(error) {
+//     res.status(500).json({
+//       message: error,
+//     });
+//   }
+// })
+
+// router.get('/:id',async (req,res)=> {
+//   const { id } = req.params;
+//   try {
+//     const result = await postModel.findById(id);
+//     res.status(200).json({
+//       message:"detail success!!",
+//       data: result,
+//     });
+//   } catch (error){
+//     res.status(500).json({
+//       message: error,
+//     })
+//   }
+// })
+
+// router.put('/:id',async (req,res)=> {
+//   const {id}=req.params;
+//   const {title, content} = req.body;
+//   try {
+//     const result = await postModel.findByIdAndUpdate(id,{
+//       title: title,
+//       content : content,
+//     },{
+//       new:true, // 변경된 사항으로 저장하는 옵션
+//     });
+//     res.status(200).json({
+//       message: "update success!!",
+//       data: result,
+//     })
+//   } catch(error) {
+//     res.status(500).json({
+//       message: error,
+//     })
+//   }
+// })
+
+// router.delete('/:id',async (req,res)=> {
+//   const {id} = req.params;
+//   try {
+//     const result =await postModel.findByIdAndDelete(id);
+//     res.status(200).json({
+//       message: "delete success!!",
+//       data: result
+//     })
+//   } catch(error) {
+//     res.status(500).json({
+//       message:error,
+//     })
+//   }
+// })
+
 module.exports = router;
